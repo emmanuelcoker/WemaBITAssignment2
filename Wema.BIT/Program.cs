6c6bd0d565321574b86221cbe2228737e20c17b8
emmanuelcoker <emmanuelcoker420@gmail.com>
2023-11-23 09:44:50 +0100

    initial commit

--- Wema.BIT/Program.cs
+++ Wema.BIT/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Newtonsoft.Json;
+using System.Numerics;
 using System.Text.Json.Serialization;
+using System.Xml.Linq;
 
 namespace Wema.BIT.User
 {
@@ -7,28 +9,31 @@ namespace Wema.BIT.User
     {
         public static void Main(string[] args)
         {
-            List<Payment> payment = new List<Payment>()
+            List<Transaction> Transaction = new List<Transaction>()
             {
-                new Payment() {Id =1,UserId=1, Amount=1233},
-                new Payment() {Id =2,UserId=2, Amount=1233},
-                new Payment() {Id =3,UserId=3, Amount=1233},
-                new Payment() {Id =4,UserId=4, Amount=1233},
-                new Payment() {Id =5,UserId=5, Amount=1233}
+
+                new Transaction() {Id =1,UserId=1, Amount=1233},
+                new Transaction() {Id =2,UserId=2, Amount=1233},
+                new Transaction() {Id =3,UserId=3, Amount=1233},
+                new Transaction() {Id =4,UserId=4, Amount=1233},
+                new Transaction() {Id =5,UserId=5, Amount=1233},
+                new Transaction() {Id =6,UserId=1, Amount=1233},
+
             };
 
             
 
             List <Users> users = new List<Users>()
             {
-                new Users() {Id =1, FirstName="Aluh", LastName="Johnson", Email="johnson@email.com", Payments=null},
+                new Users() {Id =1, FirstName="Aluh", LastName="Johnson", Email="johnson@email.com", Transactions=null},
 
-                new Users() {Id =2, FirstName="Jim", LastName="Johnson", Email="johnson@email.com", Payments=payment.Where(x => x.UserId == 2).ToList()},
+                new Users() {Id =2, FirstName="Jim", LastName="Johnson", Email="johnson@email.com", Transactions=Transaction.Where(x => x.UserId == 2).ToList()},
 
-                new Users() {Id =3, FirstName="Mike", LastName="Johnson", Email="johnson@email.com", Payments=payment.Where(x => x.UserId == 3).ToList()},
+                new Users() {Id =3, FirstName="Mike", LastName="Johnson", Email="johnson@email.com", Transactions=Transaction.Where(x => x.UserId == 3).ToList()},
 
-                new Users() {Id =4, FirstName="Jide", LastName="Johnson", Email="johnson@email.com", Payments=payment.Where(x => x.UserId == 4).ToList()},
+                new Users() {Id =4, FirstName="Jide", LastName="Johnson", Email="johnson@email.com", Transactions=Transaction.Where(x => x.UserId == 4).ToList()},
 
-                new Users() {Id =5, FirstName="Tomi", LastName="Johnson", Email="johnson@email.com", Payments=payment.Where(x => x.UserId == 5).ToList()},
+                new Users() {Id =5, FirstName="Tomi", LastName="Johnson", Email="johnson@email.com", Transactions=Transaction.Where(x => x.UserId == 5).ToList()},
                 
             };
 
@@ -57,16 +62,27 @@ namespace Wema.BIT.User
         public string FirstName { set; get; }
         public string LastName { set; get; }
         public string Email { set; get; }
-        public List<Payment> Payments { set; get; }
+        public List<Transaction> Transactions { set; get; }
     }
 
-    public class Payment
+    public class Transaction
     {
 
         public int Id { set; get; }
         public int UserId { set; get; }
         public decimal Amount { set; get; }
-        public User User { set; get; }
+        public int AccountId { set; get; }
+    }
+    public class Account
+    {
+        public int Id { set; get; }
+        public string AccountNumber { set; get; }
+        public string BVN { set; get; }
+        public string AccountName { set; get; } 
+        public string PhoneNumber {  set; get; }
+
+        public int UserId { set; get; } 
+
     }
 }
 
